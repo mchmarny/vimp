92dcbd37f308f1ac89f323005ea55b7006a3818c
Mark Chmarny <mark@chmarny.com>
2023-03-05 06:25:33 -0800

    implements batch note create

--- cmd/vulctl/cli/flag.go
+++ cmd/vulctl/cli/flag.go
@@ -9,18 +9,27 @@ import (
 )
 
 var (
+	projectFlag = &c.StringFlag{
+		Name:    "project",
+		Aliases: []string{"p"},
+		Usage:   "GCP project ID where the vulnerabilities will be imported",
+	}
+
 	sourceFlag = &c.StringFlag{
-		Name:  "source",
-		Usage: "uri of the image from which the report was generated (e.g. us-docker.pkg.dev/project/repo/img@sha256:f6efe...)",
+		Name:    "source",
+		Aliases: []string{"s"},
+		Usage:   "uri of the image from which the report was generated (e.g. us-docker.pkg.dev/project/repo/img@sha256:f6efe...)",
 	}
 
 	fileFlag = &c.StringFlag{
-		Name:  "file",
-		Usage: "path to vulnerability report to import",
+		Name:    "file",
+		Aliases: []string{"f"},
+		Usage:   "path to vulnerability report to import",
 	}
 
 	formatFlag = &c.StringFlag{
-		Name:  "format",
-		Usage: fmt.Sprintf("file type (e.g. %s, etc.)", strings.Join(types.GetSourceFormatNames(), ", ")),
+		Name:    "format",
+		Aliases: []string{"t"},
+		Usage:   fmt.Sprintf("file type (e.g. %s, etc.)", strings.Join(types.GetSourceFormatNames(), ", ")),
 	}
 )
--- cmd/vulctl/cli/import_test.go
+++ cmd/vulctl/cli/import_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/mchmarny/vulctl/pkg/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/urfave/cli/v2"
 )
@@ -15,6 +16,7 @@ func TestImport(t *testing.T) {
 	err := importCmd(c)
 	assert.Error(t, err)
 
+	set.String(projectFlag.Name, types.TestProjectID, "")
 	set.String(sourceFlag.Name, "us-docker.pkg.dev/project/repo/img@sha256:f6efe...", "")
 	set.String(fileFlag.Name, "../../../data/snyk.json", "")
 	set.String(formatFlag.Name, "snyk", "")
