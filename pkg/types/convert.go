c8d57521e20a025ebde89d12b8da9d42ecf3bde2
Mark Chmarny <mark@chmarny.com>
2023-03-07 12:54:30 -0800

    clean dead code

--- pkg/types/convert.go
+++ pkg/types/convert.go
@@ -4,10 +4,11 @@ import (
 	g "google.golang.org/genproto/googleapis/grafeas/v1"
 )
 
+// NoteOccurrences is a helper struct to hold Note and Occurrences.
 type NoteOccurrences struct {
-	// Note that the list of Occurrences points to
+	// Note that the list of Occurrences points to.
 	Note *g.Note
 
-	// Occurrences that belong to the Note
+	// Occurrences that belong to the Note.
 	Occurrences []*g.Occurrence
 }
