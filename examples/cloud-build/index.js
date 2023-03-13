f6cdbfd19ffa68ae91dca2c0b19f11e0baf3f0a1
Mark Chmarny <mark@chmarny.com>
2023-03-13 13:25:39 -0700

    gcb test

--- examples/cloud-build/index.js
+++ examples/cloud-build/index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
 
 const port = parseInt(process.env.PORT) || 8080;
 app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+  console.log(`listening on port: ${port}`);
 });
 
 module.exports = app;
