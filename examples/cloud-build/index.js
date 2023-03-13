20fb0b5c4766f71bd58ced13b818be39e4933423
Mark Chmarny <mark@chmarny.com>
2023-03-13 07:41:13 -0700

    working nodejs cloud build example

---
+++ examples/cloud-build/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const name = process.env.NAME || 'World';
+  res.send(`Hello ${name}!`);
+});
+
+const port = parseInt(process.env.PORT) || 8080;
+app.listen(port, () => {
+  console.log(`helloworld: listening on port ${port}`);
+});
+
+module.exports = app;
