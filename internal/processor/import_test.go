2c3fcb70a126d4f3592b694b6c2f4c6cf3bec306
Mark Chmarny <mark@chmarny.com>
2023-04-06 09:40:14 -0700

    version bump

--- internal/processor/import_test.go
+++ internal/processor/import_test.go
@@ -13,27 +13,9 @@ func TestImportInput(t *testing.T) {
 	assert.Error(t, o.validate())
 
 	o = &ImportOptions{
-		Source: "us-docker.pkg.dev/project/repo/img@sha256:f6efe",
-	}
-	assert.Error(t, o.validate())
-
-	o = &ImportOptions{
-		Source: "us-docker.pkg.dev/project/repo/img@sha256:f6efe",
-		File:   "../converter/grype/test.json",
-	}
-	assert.Error(t, o.validate())
-
-	o = &ImportOptions{
 		Source: "",
 		File:   "../converter/grype/test.json",
 		Target: "console://stdout",
-	}
-	assert.Error(t, o.validate())
-
-	o = &ImportOptions{
-		Source: "us-docker.pkg.dev/project/repo/img@sha256:f6efe",
-		File:   "../converter/grype/test.json",
-		Target: "",
 	}
 	assert.Error(t, o.validate())
 
